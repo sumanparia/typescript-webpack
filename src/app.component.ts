3c9e7f6826928164d0226170e4af8f0ddc2b5c02
suman.paria@gmail.com <suman.paria@gmail.com>
2017-09-07 21:22:00 -0700

    Angular 2 material added

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'my-app',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  favoriteSeason: string;
+  seasons = [
+    'Winter',
+    'Spring',
+    'Summer',
+    'Autumn',
+  ];
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdRadioModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MdRadioModule,
+    BrowserAnimationsModule
   ],
   declarations: [
     AppComponent
